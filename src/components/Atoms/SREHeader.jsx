d6583141414f2aa4f6872e39f23ec48218a2ad26
jeremydolle <j.dolle@thecodingmachine.com>
2022-01-26 15:55:18 +0100

    :recycle:(component): update the ui of Header component

--- src/components/Atoms/SREHeader.jsx
+++ src/components/Atoms/SREHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../../assets/logo-small.svg';
+import logo from '../../assets/images/logo-small.svg';
 
 function SreHeader() {
   return (
-    <div className="relative bg-white">
+    <div className="backdrop-blur-sm relative sticky top-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div
-          className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10"
+          className="flex justify-between items-center border-gray-100 py-6 md:justify-start md:space-x-10"
         >
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="#" className="flex items-center">
